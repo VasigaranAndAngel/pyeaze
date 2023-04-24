45a5163bf96e9c1bc93c2a4a8d3807d26c053cfd
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2023-04-24 18:32:40 +0530

    Renaming to pyeaze Init files added (__init__.py)

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+pyeaZe
+Author: Vasigaran
+This is made for do animation stuffs.
+Homepage: https://github.com/VasigaranAndAngel/pyeaze
+"""
+
+__version__ = '0.8'
+
+from .pyeaze import Animator
